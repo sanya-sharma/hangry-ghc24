fb876670455f7f740c2e3f6e516093eea48486e9
Aayushi Chadha <achadha@uber.com>
2024-09-26 14:28:18 -0400

    added validate order

--- wfactivities/validate_order.go
+++ wfactivities/validate_order.go
@@ -6,10 +6,20 @@ import (
 )
 
 func ValidateOrder(ctx context.Context, name string) (string, error) {
-	////////////////////////////////////////
-	// TODO: show multiple retries and the last one should succeed
-	////////////////////////////////////////
+
+	info := activity.GetInfo(ctx)
+
+	// Get a logger to log retry attempts
 	logger := activity.GetLogger(ctx)
 	logger.Info("ValidateOrder activity started")
-	return dish + "Order validated for " + customer + "!", nil
+
+	// Simulate a failure for first 2 attempts and succeed on the 3rd attempt
+	if info.Attempt < 3 {
+		logger.Info("ValidateOrder activity failed")
+		return "", nil
+	}
+
+	// Succeed on the 3rd attempt
+	logger.Info("ValidateOrder activity succeeded")
+	return "ValidateOrder for " + name + " found!", nil
 }
